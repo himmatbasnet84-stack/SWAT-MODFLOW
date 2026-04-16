b8069496a02ef66f5539467bcde44134007c031d
himmatbasnet84-stack <himmatbasnet84@gmail.com>
2026-04-16 12:21:57 -0500

    Add SWAT-MODFLOW project files

---
+++ SWAT-MODFLOW/000030009.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:96 Subbasin:3 HRU:9 Luse:FRSE Soil: 390503 Slope: 2.0-8.0 11/30/2025 12:00:00 AM ArcSWAT 2012.10_8.26
+ Soil Name: Eckrant
+ Soil Hydrologic Group: D
+ Maximum rooting depth(mm) :  300.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : CBV-C- CBV-C
+ Depth                [mm]:      100.00      300.00
+ Bulk Density Moist [g/cc]:        1.15        1.15
+ Ave. AW Incl. Rock Frag  :        0.07        0.07
+ Ksat. (est.)      [mm/hr]:        9.72        9.72
+ Organic Carbon [weight %]:        3.49        2.91
+ Clay           [weight %]:       42.00       45.00
+ Silt           [weight %]:       39.00       37.00
+ Sand           [weight %]:       19.00       18.00
+ Rock Fragments   [vol. %]:       10.00       20.00
+ Soil Albedo (Moist)      :        0.09        0.09
+ Erosion K                :        0.15        0.20
+ Salinity (EC, Form 5)    :        0.70        0.70
+ Soil pH                  :        7.30        7.30
+ Soil CACO3               :        2.00        2.00
+                              
---
+++ SWAT-MODFLOW/000050037.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:198 Subbasin:5 HRU:37 Luse:FRSE Soil: 390450 Slope: 2.0-8.0 11/30/2025 12:00:00 AM ArcSWAT 2012.10_8.26
+ Soil Name: Brackett
+ Soil Hydrologic Group: D
+ Maximum rooting depth(mm) :  410.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : GR-CL- CL
+ Depth                [mm]:      130.00      410.00
+ Bulk Density Moist [g/cc]:        1.40        1.43
+ Ave. AW Incl. Rock Frag  :        0.12        0.12
+ Ksat. (est.)      [mm/hr]:       32.40       32.40
+ Organic Carbon [weight %]:        1.16        0.73
+ Clay           [weight %]:       30.00       28.50
+ Silt           [weight %]:       37.00       40.00
+ Sand           [weight %]:       33.00       31.50
+ Rock Fragments   [vol. %]:       30.00       35.00
+ Soil Albedo (Moist)      :        0.30        0.30
+ Erosion K                :        0.24        0.28
+ Salinity (EC, Form 5)    :        1.00        1.00
+ Soil pH                  :        7.90        7.90
+ Soil CACO3               :       60.00       65.00
+                              
